494f7757577dd1a23e7abb502f17707319424a4f
Marcus Forte <davi2812@hotmail.com>
2024-08-18 23:45:36 +0200

    add RPI hello

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,15 @@
-#include <stdio.h>
 #include "stm32f10x.h"
+#include <memory>
+class MyObj {
+
+private:
+int attr_;
+};
 
 int main (){
     SystemInit();
     SystemCoreClockUpdate();
-    printf("Hello!");
+    MyObj a;
+    std::shared_ptr<MyObj> heap_obj = std::make_shared<MyObj>();
+    
 }
