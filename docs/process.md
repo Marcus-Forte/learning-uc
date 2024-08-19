393bab44b43f836edf9c0d5a2f2d1a7054d3be23
Marcus Forte <davi2812@hotmail.com>
2024-08-19 20:14:32 +0200

    HAL

---
+++ docs/process.md
@@ -0,0 +1,4 @@
+# Thought Process
+1. Pick MCU
+2. Dependencies: CMSIS Core, CMSIS Device, HAL
+3. Create HAL CONF
--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,31 @@
-#include "stm32f10x.h"
+#include "stm32f103xb.h"
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_gpio.h"
 #include <memory>
-class MyObj {
 
-private:
-int attr_;
+class MyObj {
+    public:
+    private:
+    int attr_;
+};
+ GPIO_InitTypeDef LedGPIO {
+    .Pin = GPIO_PIN_13,
+    .Mode = GPIO_MODE_OUTPUT_PP,
+    .Pull = GPIO_NOPULL,
+    .Speed = GPIO_SPEED_FREQ_LOW
 };
 
 int main (){
-    SystemInit();
-    SystemCoreClockUpdate();
-    MyObj a;
-    std::shared_ptr<MyObj> heap_obj = std::make_shared<MyObj>();
+    std::shared_ptr<MyObj> heap_obj;
+    // heap_obj = std::make_shared<MyObj>();
+    HAL_Init();
+    HAL_GPIO_Init(GPIOC, &LedGPIO);
+
+    while(true) {
+        HAL_Delay(1000);
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+         HAL_Delay(1000);
+         HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    }
     
 }
