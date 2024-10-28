4efa3bd96811d63bb8c257d827e1437479e65c36
root <davi2812@hotmail.com>
2024-10-28 01:57:08 +0000

    tests

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,9 @@
 
 
 #include "LED.hh"
+#include "Module.hh"
+#include "Timing.hh"
 #include "UART.hh"
-
 #include "stm32f1xx_hal.h"
 
 #include <vector>
@@ -19,24 +20,18 @@ int main() {
   __GPIOA_CLK_ENABLE();
   __USART1_CLK_ENABLE();
 
-  // auto led = std::make_unique<LED>(GPIOC, GPIO_PIN_13);
-  // auto uart = std::make_unique<UART>(USART1);
-  auto *led = new LED(GPIOC, GPIO_PIN_13);
-  auto *uart = new UART(USART1);
-  std::vector<int> myvec;
-
+  auto led = std::make_shared<LED>(GPIOC, GPIO_PIN_13);
+  auto uart = std::make_shared<UART>(USART1);
+  auto timing = std::make_shared<Timing>();
+  // auto *led = new LED(GPIOC, GPIO_PIN_13);
+  // auto *uart = new UART(USART1);
+  // std::vector<int> myvec;
   uint32_t count = 0;
   char buff[50];
+  Module module(led, uart, timing);
   while (1) {
     HAL_Delay(50);
-    count++;
-    led->toggle();
-    uart->write("Hello! " + std::to_string(count));
-
-    const auto read = uart->read();
-    if (!read.empty()) {
-    }
+    module.blink(1000);
+    module.send();
   }
-  delete led;
-  delete uart;
 }
